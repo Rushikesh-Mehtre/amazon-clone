51b510c468c04baf0465068c83b284fb3be7d087
Rushikesh-Mehtre <rsmrsm5952@gmail.com>
2022-04-20 20:22:17 +0530

    started on subheader components

---
+++ src/components/SubHeadBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/SubHeadBar.module.scss";
+const SubHeadBar = () => {
+  console.log("into subhead bar");
+  return <div className={styles.subHeadBar}>SubHeadBar</div>;
+};
+
+export default SubHeadBar;
