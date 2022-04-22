7d120fec2fbcf0514ebc5d0d6cf7e4cf36bd5246
Rushikesh-Mehtre <rsmrsm5952@gmail.com>
2022-04-23 00:15:33 +0530

    started on products slider feature

---
+++ src/components/ProductsSlider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "../styles/ProductsSlider.module.scss";
+const ProductsSlider = () => {
+  return <div className={styles.productsSlider}>ProductsSlider</div>;
+};
+
+export default ProductsSlider;
